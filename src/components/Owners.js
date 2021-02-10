f098d1e48695b450441863572c73eba0e146f001
dnodolf1212 <dnodolf@gmail.com>
2021-02-09 17:47:39 -0800

    console.log(props) to make sure I had info coming in from my fetch request.

--- src/components/Owners.js
+++ src/components/Owners.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Owners = (props) => {
+  console.log(props)
   return (
-    <>
+    <div>
       {props.owners && props.owners.map( owner => 
-      <div key={owner.id}> 
-        <Link to={ `/owners/${owner.id}` }>{owner.ownername}</Link> 
-      </div> )}
-    </>
+        <div key={owner.id}> 
+          <Link to={ `/owners/${owner.id}` }>{owner.ownername}</Link> 
+        </div> )}
+    </div>
   )
 }
 export default Owners
