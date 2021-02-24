237a5b32e2c188f0de8a77b82ce5f70576c861dd
dnodolf1212 <dnodolf@gmail.com>
2021-02-23 19:21:49 -0800

    last styling edit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App" style={{ backgroundColor: "#8D918D", width: "100%", height: "100%", textAlign: "left", color: "white"}}>
+      <div className="App" 
+      style={{ backgroundColor: "#8D918D", 
+              width: "100%", 
+              height: "100%", 
+              textAlign: "left", 
+              color: "white"}}
+            >
         <h3 style={{padding: "10px"}}>Hi, Primary User.</h3>
-        <img src={nikiri} alt="aknife" style={{display: "block", marginLeft: "auto", marginRight: "auto"}}/>
+
+        <img src={nikiri} alt="aknife" 
+        style={{display: "block", 
+                marginLeft: "auto", 
+                marginRight: "auto"}}
+              />
+
         <Header />
         
       </div>
